c7f34b980145ef6d32e67e14228ccff68b2f7252
Sahil Sheikh <you@example.com>
2026-09-04 15:27:29 +0530

    Fix dropdown menu

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -55,7 +55,7 @@ const Navbar = () => {
 
                {/* Dropdown Menu */}
                {token &&
-                  <div className={`absolute dropdown-menu right-0 pt-4 ${showDropdown ? 'block' : 'hidden'} md:group-hover:block`}>
+                  <div className={`absolute dropdown-menu right-0 pt-4 ${showDropdown ? 'block' : 'hidden'}`}>
                      <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500">
                         <p className='cursor-pointer hover:text-black'>My Profile</p>
                         <p onClick={() => {
